6b0344d4f5fe79b8d8d33ab952e8eba0ac57d722
magicjar <9734293+magicjar@users.noreply.github.com>
2020-06-26 09:36:39 +0700

    UMD only

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,7 @@ export default {
     input: 'src/js/magic-lang.js',
     output: [
         {
-            file: 'dist/js/cjs/magic-lang.js',
-            format: 'cjs',
-            name: 'MagicLang'
-        }, {
-            file: 'dist/js/esm/magic-lang.mjs',
-            format: 'es',
-            name: 'MagicLang'
-        }, {
-            file: 'dist/js/umd/magic-lang.js',
+            file: 'dist/js/magic-lang.js',
             format: 'umd',
             name: 'MagicLang'
         }
