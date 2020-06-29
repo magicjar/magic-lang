97c523515aff420d3f4af29ffa75ea1239842299
magicjar <9734293+magicjar@users.noreply.github.com>
2020-06-29 13:10:42 +0700

    Update compiling workflow

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,23 @@
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+
 export default {
-    input: 'src/js/magic-lang.js',
+    input: 'src/magic-lang.ts',
     output: [
         {
             file: 'dist/js/magic-lang.js',
-            format: 'umd',
+            format: 'iife',
             name: 'MagicLang'
+        }, {
+            file: 'dist/js/magic-lang.min.js',
+            format: 'iife',
+            name: 'MagicLang',
+            plugins: [terser()]
         }
     ],
     plugins: [
+        typescript({
+            typescript: require('typescript')
+        })
     ]
 }
